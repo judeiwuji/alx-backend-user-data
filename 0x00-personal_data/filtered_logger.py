003441ba5b8b0b2ee2e2b0a2b320e67056ecfd82
judeiwuji <judeiwuji@gmail.com>
2023-04-26 18:07:08 +0100

    update

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,13 +1,35 @@
 #!/usr/bin/env python3
 """Module: Filtered Logger"""
 from typing import List, Dict
+import logging
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        filtered = filter_datum(self.fields, self.REDACTION,
+                                record.getMessage(), self.SEPARATOR).\
+            replace(";", "; ")
+        record.msg = filtered
+        return super().format(record)
 
 
 def parse_data(data: str, sep: str) -> Dict[str, str]:
     """parse message into dict"""
     if data is None:
         return {}
-    return {d.split('=')[0]: d.split('=')[1] for d in data.split(sep) if d}
+    return {d.split('=')[0]: d.split('=')[1]
+            for d in data.split(sep) if d and '=' in d}
 
 
 def filter_datum(fields: List[str], redaction: str,
