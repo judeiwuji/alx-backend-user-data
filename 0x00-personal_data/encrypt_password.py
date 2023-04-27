abb989270d95f6e7b9e1231b2873409b33af2915
judeiwuji <judeiwuji@gmail.com>
2023-04-27 13:42:43 +0100

    update

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python3
 """Module: Encrypt password"""
-from bcrypt import gensalt, hashpw, checkpw
+import bcrypt
 
 
 def hash_password(password: str) -> bytes:
     """hash a given password"""
-    salt = gensalt(12)
-    return hashpw(password.encode("utf-8"), salt)
+    salt = bcrypt.gensalt(12)
+    return bcrypt.hashpw(password.encode("utf-8"), salt)
 
 
 def is_valid(hashed_password: bytes, password: str) -> bool:
-    """validate that the provided password matches\
+    """validate that the provided password matches
     the hashed password."""
-    return checkpw(password.encode("utf-8"), hashed_password)
+    return bcrypt.checkpw(password.encode("utf-8"), hashed_password)
