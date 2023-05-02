3d10c4cf6e86bc8d49238c1c8014aea7158a96ed
judeiwuji <judeiwuji@gmail.com>
2023-05-02 16:18:37 +0100

    Add: Task 8

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,6 +3,7 @@
 Basic Authentication module
 """
 from api.v1.auth.auth import Auth
+from base64 import b64decode
 
 
 class BasicAuth(Auth):
@@ -23,3 +24,21 @@ class BasicAuth(Auth):
         if not authorization_header.startswith('Basic '):
             return None
         return authorization_header.split(" ")[1]
+
+    def decode_base64_authorization_header(
+            self,
+            base64_authorization_header: str) -> str:
+        """Decodes a basic auth token
+        Return
+         - the decoded token
+        """
+        if base64_authorization_header is None:
+            return None
+        if type(base64_authorization_header) is not str:
+            return None
+
+        try:
+            return b64decode(base64_authorization_header).\
+                decode("utf-8")
+        except Exception:
+            return None
