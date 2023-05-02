1fc9394e86c86ff77ed11d66b7804baf7a1af83c
judeiwuji <judeiwuji@gmail.com>
2023-05-02 15:05:17 +0100

    Add: task 1

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -24,3 +24,12 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+
+@app_views.route('/unauthorized', strict_slashes=False)
+def unauthorized():
+    """ GET /api/v1/unauthorized
+    Return:
+      - None
+    """
+    abort(401)
