e0cbc7a6dd9688b7216b4f0b23eebcd60417747c
judeiwuji <judeiwuji@gmail.com>
2023-05-10 16:03:35 +0100

    Add: Task 12

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -5,6 +5,7 @@ from bcrypt import hashpw, gensalt, checkpw
 from sqlalchemy.orm.exc import NoResultFound
 from user import User
 import uuid
+from typing import Union
 
 
 def _hash_password(password: str) -> bytes:
@@ -60,3 +61,13 @@ class Auth:
         session_id = _generate_uuid()
         self._db.update_user(user.id, session_id=session_id)
         return session_id
+
+    def get_user_from_session_id(self, session_id) -> Union[User, None]:
+        """
+        Returns
+         - the corresponding User or None
+        """
+        try:
+            return self._db.find_user_by(session_id=session_id)
+        except NoResultFound:
+            return None
